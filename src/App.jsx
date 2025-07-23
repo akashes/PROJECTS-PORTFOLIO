e0a94abf3711c100a15333275035c9fdde32e717
akash <akashes5753279@gmail.com>
2025-07-23 23:56:45 +0530

    hero section, about section,skills section and services section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
 import { useState } from 'react'
-
+import Header from './components/header/Header'
 import './App.css'
+import Home from './components/home/Home'
+import About from './components/about/About'
+import Skills from './components/skills/Skills'
+import Services from './components/services/Services'
+import Qualification from './components/qualification/Qualification'
 
 function App() {
 
   return (
   <>
+   <Header/>
+   <main className="main">
+
+   <Home/>
+   <About/>
+   <Skills/>
+   <Services/>
+   <Qualification/>
+   </main>
+ 
+   
   </>
   )
 }
